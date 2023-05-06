3f5c91fcc03e16f68d056d7e56cd6dbd42c43945
Jake Conwell <jakelconwell@gmail.com>
2023-05-06 08:06:42 -0500

    Added validation to controller for name, description and email.

--- src/main/java/org/launchcode/codingevents/models/Event.java
+++ src/main/java/org/launchcode/codingevents/models/Event.java
@@ -1,5 +1,8 @@
 package org.launchcode.codingevents.models;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 import java.util.Objects;
 
 public class Event {
@@ -8,15 +11,20 @@ public class Event {
 
     private static int nextId = 1;
 
-
+    @NotBlank
+    @Size(min = 3, max = 50, message="Name must be between 3 and 50 characters long.")
     private String name;
-
+    @Size(max = 500, message="Description too long.")
     private String description;
+    @NotBlank
+    @Email(message="Invalid Email. Try again.")
+    private String contactEmail;
 
-    public Event(String name, String description) {
-        this.id = nextId;
+    public Event(String name, String description, String contactEmail) {
         this.name = name;
         this.description = description;
+        this.contactEmail = contactEmail;
+        this.id = nextId;
         nextId++;
     }
 
@@ -38,6 +46,14 @@ public class Event {
 
     public int getId() {
         return id;
+    }
+
+    public String getContactEmail() {
+        return contactEmail;
+    }
+
+    public void setContactEmail(String contactEmail) {
+        this.contactEmail = contactEmail;
     }
 
     @Override
