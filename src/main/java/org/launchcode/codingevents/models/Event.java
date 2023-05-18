239b81add6fc5e054d15473811e0609bd3a85d0c
jlconwell <jakelconwell@gmail.com>
2023-05-18 11:13:14 -0500

    linked MySQL

--- src/main/java/org/launchcode/codingevents/models/Event.java
+++ src/main/java/org/launchcode/codingevents/models/Event.java
@@ -1,11 +1,17 @@
 package org.launchcode.codingevents.models;
 
-import javax.validation.constraints.*;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 import java.util.Objects;
-
 /**
  * Created by Chris Bay
  */
+
+@Entity
 public class Event {
 
     private int id;
